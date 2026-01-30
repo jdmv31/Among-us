f69c8f1654193d73c68b0b8713b8f1a31c2e0eca
jdmv31 <josuemedina.mv@gmail.com>
2026-01-30 18:24:45 -0400

    cositas agregadas

---
+++ src/amongUs/Impostor.java
@@ -0,0 +1,8 @@
+package amongUs;
+
+public class Impostor extends Personaje{
+
+    public void realizarTarea(){
+        System.out.println("lol");
+    }
+}
---
+++ src/amongUs/Personaje.java
@@ -0,0 +1,10 @@
+package amongUs;
+
+public abstract class Personaje {
+    protected int posX, posY;
+    protected String color;
+    protected String username;
+    protected boolean estaVivo;
+
+    public abstract void realizarTarea();
+}
