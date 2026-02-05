ca7a99674d047fa55f7a9d6f7343b3e29bdf537a
jdmv31 <josuemedina.mv@gmail.com>
2026-02-04 22:39:20 -0400

    cambios

--- src/main/java/amongUs/MenuController.java
+++ src/main/java/amongUs/MenuController.java
@@ -5,8 +5,14 @@ import com.almasb.fxgl.ui.UIController; // Importante para que FXGL lo reconozca
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import static com.almasb.fxgl.dsl.FXGL.*;
+import com.almasb.fxgl.audio.Sound;
+import javafx.scene.layout.Pane;
 
 public class MenuController implements UIController {
+
+    @FXML private Pane ventanaModal;
+    @FXML private Pane ventanaModal1;
 
     @Override
     public void init() {
@@ -35,13 +41,41 @@ public class MenuController implements UIController {
     }
 
     @FXML
+    private void botonPresionado(){
+        Sound sonido = FXGL.getAssetLoader().loadSound("clickBoton.mp3");
+        getAudioPlayer().playSound(sonido);
+    }
+
+    @FXML
     private void onCreditos(){
+        botonPresionado();
         cambiarEscena("/ui/creditos.fxml");
     }
 
     @FXML
     private void onVolverMenu() {
+        botonPresionado();
         cambiarEscena("/ui/menuPrincipal.fxml");
+    }
+
+    @FXML
+    private void onUnirsePartida(){
+        botonPresionado();
+        ventanaModal1.setVisible(false);
+        ventanaModal.setVisible(true);
+    }
+
+    @FXML
+    private void onCrearPartida(){
+        botonPresionado();
+        ventanaModal.setVisible(false);
+        ventanaModal1.setVisible(true);
+    }
+
+    @FXML
+    private void onJugar(){
+        botonPresionado();
+        cambiarEscena("/ui/jugar.fxml");
     }
 
     @FXML
