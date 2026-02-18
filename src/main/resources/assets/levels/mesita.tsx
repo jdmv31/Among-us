6fa8f5579d61084da24d564adedc054ffe13d6bc
jdmv31 <josuemedina.mv@gmail.com>
2026-02-18 17:03:51 -0400

    a

---
+++ src/main/resources/assets/levels/mesita.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="mesita" tilewidth="32" tileheight="32" spacing="2" margin="2" tilecount="900" columns="30">
+ <image source="mesita.png" width="1024" height="1024"/>
+</tileset>
---
+++ src/main/resources/assets/levels/muebles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="muebles" tilewidth="32" tileheight="32" spacing="3" margin="3" tilecount="180" columns="15">
+ <image source="PostApoc_Workshop.png" width="544" height="448"/>
+</tileset>
