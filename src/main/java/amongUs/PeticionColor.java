2e7e0353b16cbc7d6f40c4cac1e2264f9eeff09a
Sebastian-Arismendi <sebastianjose.arismendi.2021@gmail.com>
2026-03-11 15:07:26 -0400

    Comentarios a los archivos de Nodo alcantarilla, peticion color, reunion, unirse, reporte component, resultado de votos y reunioncontroll

--- src/main/java/amongUs/PeticionColor.java
+++ src/main/java/amongUs/PeticionColor.java
@@ -1,5 +1,14 @@
 package main.java.amongUs;
+/**
+ * Representa una solicitud o paquete de datos enviado a través de la red.
+ * Se encarga que un jugador pueda seleccionar o cambiar el color
+ * de su personaje en la sala de espera (lobby) antes de iniciar una partida.
+ *
+ * @author Sebastián Arismendi
+ */
 
 public class PeticionColor {
+    /** nombre o código hexadecimal del color seleccionado por el jugador
+     */
     public String color;
 }
